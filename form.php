4e9ce4e1b16587cd62b1ab820429c7a8d9838973
lalabhi <abhishek.lal@qed42.com>
2019-01-18 13:39:32 +0530

    catching forms in php

---
+++ form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Learn PHP</title>
+</head>
+<body>
+<h1>Lets do php</h1>
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+
+
+    
+</body>
+</html>
