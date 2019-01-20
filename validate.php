8f1f1c0c6ba3b393b18071a2ccd9b09c2fb3ef61
lalabhi <abhishek.lal@qed42.com>
2019-01-20 23:31:23 +0530

    errors resolvedgit add validate.php

--- validate.php
+++ validate.php
@@ -10,7 +10,7 @@ $eerror = $eemail="";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
+    $eerror = "Name is required";
   } else {
     $name = test_input($_POST["name"]);
     if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
