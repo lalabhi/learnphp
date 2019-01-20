9fb9acf71244422158b855b77f5a51002d233831
lalabhi <abhishek.lal@qed42.com>
2019-01-20 23:20:49 +0530

    added error messages and validated emails

--- validate.php
+++ validate.php
@@ -4,28 +4,9 @@
 </head>
 <body>  
 
-
-
-<h2>PHP Form Validation Example</h2>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-  Name: <input type="text" name="name"><span><?php  echo $nerror;?></span>
-  <br><br>
-  E-mail: <input type="text" name="email">
-  <br><br>
-  Website: <input type="text" name="website">
-  <br><br>
-  Comment: <textarea name="comment" rows="5" cols="40"></textarea>
-  <br><br>
-  Gender:
-  <input type="radio" name="gender" value="female">Female
-  <input type="radio" name="gender" value="male">Male
-  <input type="radio" name="gender" value="other">Other
-  <br><br>
-  <input type="submit" name="submit" value="Submit">  
-</form>
 <?php
 $name = $email = $gender = $comment = $website = "";
-$nerror=$eerror="";
+$eerror = $eemail="";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if(empty($_POST["name"])){
@@ -34,7 +15,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   else{
   $name = test_input($_POST["name"]);
   }
-  $email = test_input($_POST["email"]);
+  if(empty($_POST["email"])){
+    $eemail="u can't give empty email";
+  }
+  else{
+    $email = test_input($_POST["email"]);
+    if (!strpos($email, '@')) {
+      $eemail="Not a valid email";
+      $email=" ";
+      reload();
+  }
+  }
   $website = test_input($_POST["website"]);
   $comment = test_input($_POST["comment"]);
   $gender = test_input($_POST["gender"]);
@@ -52,6 +43,25 @@ function test_input($anand) {
   //  echo"<script>alert('cant be empty');</script>";
 //}
 ?>
+
+<h2>PHP Form Validation Example</h2>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name"> <span> <?php echo $eerror;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email"><span> <?php echo $eemail;?></span>
+  <br><br>
+  Website: <input type="text" name="website">
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" value="female">Female
+  <input type="radio" name="gender" value="male">Male
+  <input type="radio" name="gender" value="other">Other
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
 <?php
 echo "<h2>Your Input:</h2>";
 echo $name;
