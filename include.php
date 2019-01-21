02341c9967c37b9fe3f00deaf8a078bd1bc2a12c
lalabhi <abhishek.lal@qed42.com>
2019-01-21 09:51:38 +0530

    file handling startinggit add include.php dict.txt

--- include.php
+++ include.php
@@ -1,12 +1,16 @@
 <html>
 <body>
-
+<!--
 <div class="form">
-<?php include 'validate.php';?>
+<//?php include 'validate.php';?>
 </div>
-
+-->
 <h1>Welcome to my page!</h1>
 </p>
+
+<?php 
+    echo readfile("dict.txt");
+?>
 
 </body>
 </html>
